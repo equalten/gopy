a4b2c8257b8c3a84ca96343fafb60442ab447f1c
equalten <equalten@gmail.com>
2021-02-11 21:45:24 +0900

    add rpc

--- rpc3.py
+++ rpc3.py
@@ -7,5 +7,5 @@ dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 dev.open()
 
 inter_info = dev.rpc.get_interface_information()
-print(etree.tostring(inter_info, encoding='utf8', method='xml'))
+print(etree.tostring(inter_info, encoding='utf8', method='xml', xml_declaration=True))
 dev.close()
