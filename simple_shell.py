45d80f89de6144d5843a70071c3c1d12bc8bcf42
equalten <equalten@gmail.com>
2021-02-11 11:33:12 +0900

    add shell

--- simple_shell.py
+++ simple_shell.py
@@ -3,7 +3,7 @@ from jnpr.junos.utils.start_shell import StartShell
 
 dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 
-ss = StartShell()
+ss = StartShell(dev)
 ss.open()
 version = ss.run('cli -c "show version"')
 print(version)
