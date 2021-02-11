96a96dee2026ec5c574f8eba481e517874f8cf27
equalten <equalten@gmail.com>
2021-02-11 12:43:52 +0900

    add rpc

--- rpc1.py
+++ rpc1.py
@@ -3,4 +3,4 @@ from lxml import etree
 
 with Device(host='172.27.14.72', user='jun', passwd='jun2per') as dev:
     sw_version = dev.rpc.get_software_information()
-    print(etree.tostring(sw_version))
+    print(etree.tostring(sw_version, encoding='unicode', method='xml'))
