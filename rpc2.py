236b37b2fc527dd4864270bc1f6bd542d591465e
equalten <equalten@gmail.com>
2021-02-11 21:31:04 +0900

    add rpc

--- rpc2.py
+++ rpc2.py
@@ -1,12 +1,13 @@
 from jnpr.junos import Device
 from lxml import etree
+from pprint import pprint
 
 dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 
 dev.open()
 
 sw_version_text = dev.rpc.get_software_information({'format':'text'})
-print(etree.tostring(sw_version))
+print(etree.tostring(sw_version_text, encoding='unicode' ))
 
 sw_version_json = dev.rpc.get_software_information({'format':'json'})
 pprint(sw_version_json)
---
+++ rpc3.py
@@ -0,0 +1,11 @@
+from jnpr.junos import Device
+from lxml import etree
+
+dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
+
+dev.open()
+
+inter_info = dev.rpc.get_interface_information()
+print(etree.tostring(inter_info))
+
+dev.close()
