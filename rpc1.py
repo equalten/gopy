f13549e696acd75a03c9d30014ce85a369bc415f
equalten <equalten@gmail.com>
2021-02-11 12:42:02 +0900

    add rpc

---
+++ rpc1.py
@@ -0,0 +1,6 @@
+from jnpr.junos import Device
+from lxml import etree
+
+with Device(host='172.27.14.72', user='jun', passwd='jun2per') as dev:
+    sw_version = dev.rpc.get_software_information()
+    print(etree.tostring(sw_version))
