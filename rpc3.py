ac9f919013f96b47df20c9adf61e028787aadb87
equalten <equalten@gmail.com>
2021-02-11 21:38:16 +0900

    add rpc

--- rpc3.py
+++ rpc3.py
@@ -1,11 +1,13 @@
 from jnpr.junos import Device
 from lxml import etree
+import xml.dom.minidom
 
 dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 
 dev.open()
 
 inter_info = dev.rpc.get_interface_information()
-print(etree.tostring(inter_info))
+dom = xml.dom.minidom.parse(inter_info)
+print(dom.toprettyxml())
 
 dev.close()
