db9372402f624e741655994864a3023d518cea31
equalten <equalten@gmail.com>
2021-02-11 11:48:29 +0900

    add shell

--- simple_shell.py
+++ simple_shell.py
@@ -6,5 +6,6 @@ dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 ss = StartShell(dev)
 ss.open()
 version = ss.run('cli -c "show version"')
-print(version)
+for item in version:
+    print(item)
 ss.close()
