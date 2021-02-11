b88a8f8a2686378b9e4b3258f5763b661b45c190
equalten <equalten@gmail.com>
2021-02-11 12:20:40 +0900

    add rpc

--- simple_rpc.py
+++ simple_rpc.py
@@ -4,4 +4,6 @@ dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
 
 dev.open()
 print(dev.display_xml_rpc('show route', format='text'))
+route = dev.rpc.get_route_information()
+print(route)
 dev.close()
