62987214b6f4c6161b48cfdc7d4fe49509877f81
equalten <equalten@gmail.com>
2021-02-11 11:44:24 +0900

    added shell

--- simple_shell.py
+++ simple_shell.py
@@ -1,7 +1,7 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.start_shell import StartShell
 
-dev = Device(host='172.27.14.72', user='jun', passwd='jun2per')
+dev = (host='172.27.14.72', user='jun', passwd='jun2per')
 
 ss = StartShell(dev)
 ss.open()
